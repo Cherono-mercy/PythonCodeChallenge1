9356c80d65c7c4cca30c8636ad5ca77e027a39ba
Cherono-mercy <cheronom142@gmail.com>
2023-11-27 10:56:01 +0800

    Adding call functionality to the function in challenge

--- challenges/challenge1.py
+++ challenges/challenge1.py
@@ -5,13 +5,18 @@ def convert_to_24hr():
     minute = int(input("Enter the minute(0 to 59): "))
     period = input("Enter 'am' or 'pm': ").lower()
 
+    # Edge cases for midnight and hours after midday
     if period == 'am' and hour == 12:
-        #Midnight
-        hour == 0
+        # Midnight
+        hour = 0
+        # For hours past noon, add 12 to the hour to covert it to 24-hour system
     elif period == 'pm' and hour != 12:
-        #Midday
-        hour += 12   
 
-    #Format the result as a four-digit string
-    result = f"{hour:02d}{minute:02d}"  
-    return result  
+        hour += 12
+
+    # Format the result as a four-digit string
+    result = f"{hour:02d}{minute:02d}"
+    print(result)
+
+ # Calling the function 
+convert_to_24hr()
